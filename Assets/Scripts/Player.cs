d48332eafffa2127b87098ce80717a2ea0e15f6b
kjm09139 <164484928+kjm09139@users.noreply.github.com>
2024-10-06 19:55:41 +0900

    몬스터 만드는중

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float soundVolume = 1f; // 사운드의 세기
+    public AudioSource audioSource; // 소리를 재생할 AudioSource
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,28 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    public AudioSource backgroundMusic; // 배경음악 소스
-    public AudioSource[] soundEffects; // 효과음 소스
+    // 오디오 클립 저장소
+    public AudioClip[] bgmClips; // 배경음악 클립 배열
+    public AudioClip[] sfxClips; // 효과음 클립 배열
+    public AudioSource bgmSource; // 배경음 재생 오디오 소스
+    public AudioSource sfxSource; // 효과음 재생 오디오 소스
 
-    void Start()
+    // 현재 재생 중인 BGM 클립을 추적
+    private AudioClip currentBGM;
+
+    // 배경음악 재생
+    public void PlayBGM(int bgmIndex, bool loop = true)
     {
-        PlayBackgroundMusic();
+        if (bgmIndex < 0 || bgmIndex >= bgmClips.Length) return;
+
+        AudioClip bgmClip = bgmClips[bgmIndex];
+        if (currentBGM == bgmClip) return; // 이미 같은 BGM이 재생 중이면 패스
+
+        bgmSource.clip = bgmClip;
+        bgmSource.loop = loop;
+        bgmSource.Play();
+        currentBGM = bgmClip;
     }
 
-    public void PlayBackgroundMusic()
+    // 효과음 재생 (단일 사운드)
+    public void PlaySFX(int sfxIndex)
     {
-        // 배경음악 재생
-        backgroundMusic.Play();
-    }
+        if (sfxIndex < 0 || sfxIndex >= sfxClips.Length) return;
 
-    public void PlaySoundEffect(int index)
-    {
-        // 효과음 재생
-        if (index >= 0 && index < soundEffects.Length)
-        {
-            soundEffects[index].Play();
-        }
+        sfxSource.PlayOneShot(sfxClips[sfxIndex]);
     }
 }
