ab77207275351aa00c4c7b2946f4ea53092d372a
03son <ehdalsh3@naver.com>
2024-10-27 17:22:10 +0900

    수정

--- Assets/Scripts/Managers/PhotonManager.cs
+++ Assets/Scripts/Managers/PhotonManager.cs
@@ -67,7 +67,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        room_Player = GameObject.Find("room_Player").transform;
         roomSetting = BG.GetComponent<RoomManager>();
     }
 
