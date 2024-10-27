d8c7972e92a4e7585f3b54f594ba19fe3026251a
03son <ehdalsh3@naver.com>
2024-10-27 17:45:27 +0900

    Revert "병합 충돌 제거"
    This reverts commit 544ae9e5b2abb5385f98eb45f8e2e56329f2f7d0.

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // 수집한 아이템을 저장하는 리스트
+    public List<Item> inventory = new List<Item>();
+
+    // 아이템을 줍는 메서드
+    public void PickUpItem(Item item)
+    {
+        if (item != null)
+        {
+            inventory.Add(item); // 아이템을 인벤토리 리스트에 추가
+            Debug.Log(item.itemName + " 을(를) 주웠습니다.");
+            item.gameObject.SetActive(false); // 아이템을 줍고 나서 씬에서 비활성화
+        }
+    }
+}
