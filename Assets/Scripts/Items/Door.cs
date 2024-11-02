03f8b6531a37ecac42772adde6b30ac588e2498f
kjm09139 <164484928+kjm09139@users.noreply.github.com>
2024-11-02 14:48:46 +0900

    문 여닫기 수정 (문의 잠김을 해제후 사용)

--- Assets/Scripts/Items/Door.cs
+++ Assets/Scripts/Items/Door.cs
@@ -4,6 +4,7 @@ public class Door : MonoBehaviour, IInteractable
 {
     private Animator animator;
     private bool isOpen = false;
+    private bool openDoor = false;
     private Player_Equip playerEquip; // Player_Equip 스크립트 참조
 
     void Start()
@@ -14,15 +15,33 @@ public class Door : MonoBehaviour, IInteractable
 
     public void OnInteract()
     {
+        if (openDoor == true)
+        {
+            OpenDoor();
+            Debug.Log("문이 열립니다.");
+            openDoor = false;
+            return;
+        }
         // 문이 열려있지 않고, Player_Equip에서 열쇠가 장착된 상태인지 확인
         if (!isOpen && playerEquip != null && playerEquip.HasEquippedKey())
         {
-            Debug.Log("문이 열렸습니다.");
-            OpenDoor();
+            isOpen = true;
+            Debug.Log("문의 잠금을 해제했습니다.");
+            //OpenDoor();
+            // 문의 잠금상태를 열림으로 변경
+            openDoor = true;
+            return;
         }
         else if (!isOpen)
         {
             Debug.Log("잠긴 문입니다. 열쇠가 필요합니다.");
+        }
+        if (openDoor == false)
+        {
+            Closedoor();
+            Debug.Log("문이 닫힙니다.");
+            openDoor = true;
+            return;
         }
     }
 
@@ -33,7 +52,6 @@ public class Door : MonoBehaviour, IInteractable
 
     void OpenDoor()
     {
-        isOpen = true;
         if (animator != null)
         {
             animator.SetBool("isOpen", true); // 문 열기 애니메이션 실행
@@ -42,6 +60,18 @@ public class Door : MonoBehaviour, IInteractable
         {
             // Animator가 없는 경우 문을 열리는 방향으로 이동시키는 예시 (간단한 이동)
             transform.position += -transform.forward * 1.5f; // 문을 오른쪽으로 약간 이동
+        }
+    }
+    void Closedoor()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("isClose", true); // 문 닫기 애니메이션 실행
+        }
+        else
+        {
+            // Animator가 없는 경우 문을 열리는 방향으로 이동시키는 예시 (간단한 이동)
+            transform.position -= -transform.forward * 1.5f; // 문을 왼쪽으로 약간 이동
         }
     }
 }
