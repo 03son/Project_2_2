9bc22bda9a008b65021043e8f91957f3cc1932ec
ljh2002ljh <aeb1119@naver.com>
2024-11-10 22:19:00 +0900

    1

--- Assets/Scripts/Items/MedkitTimerUI.cs
+++ Assets/Scripts/Items/MedkitTimerUI.cs
@@ -1,50 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using Photon.Pun;
 using System.Collections;
+using TMPro;
 
-public class MedkitTimerUI : MonoBehaviourPun, IPunObservable
+public class MedkitTimerUI : MonoBehaviour
 {
     public Image timerImage;
     public TextMeshProUGUI timerText;
     private float totalTime = 5f;
-    private float currentTime;
-    private bool isTimerRunning = false; // 타이머가 실행 중인지 여부를 추적하는 변수
 
     private void Start()
     {
-        // 게임 시작 시 UI 비활성화
         timerImage.gameObject.SetActive(false);
         timerText.gameObject.SetActive(false);
     }
 
-    public void TriggerMedkitTimer()
+    public void StartMedkitTimer()
     {
-        if (!isTimerRunning) // 타이머가 실행 중이 아닐 때만 실행
-        {
-            Debug.Log("TriggerMedkitTimer 메서드 실행 중");
-            isTimerRunning = true; // 타이머 실행 상태 설정
-            currentTime = totalTime;
-            timerImage.gameObject.SetActive(true);
-            timerText.gameObject.SetActive(true);
-            StartCoroutine(RunTimer());
-        }
-    }
-
-    [PunRPC]
-    public void RPC_TriggerMedkitTimer()
-    {
-        TriggerMedkitTimer();
+        Debug.Log("StartMedkitTimer 메서드 실행됨");
+        StartCoroutine(RunTimer());
     }
 
     private IEnumerator RunTimer()
     {
-        Debug.Log("RunTimer 코루틴 시작");
+        float timeLeft = totalTime;
         timerImage.gameObject.SetActive(true);
         timerText.gameObject.SetActive(true);
 
-        float timeLeft = totalTime;
         while (timeLeft > 0)
         {
             timeLeft -= Time.deltaTime;
@@ -53,35 +35,17 @@ public class MedkitTimerUI : MonoBehaviourPun, IPunObservable
             yield return null;
         }
 
-        Debug.Log("RunTimer 코루틴 종료");
-
-        // 타이머가 끝나면 UI 비활성화
         timerImage.fillAmount = 0;
         timerText.text = "0";
         timerImage.gameObject.SetActive(false);
         timerText.gameObject.SetActive(false);
-        isTimerRunning = false; // 타이머 실행 상태 초기화
     }
 
     public void ResetTimer()
     {
-        currentTime = 0;
         timerImage.fillAmount = 0;
         timerText.text = "";
         timerImage.gameObject.SetActive(false);
         timerText.gameObject.SetActive(false);
-        isTimerRunning = false; // 타이머 실행 상태 초기화
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(currentTime);
-        }
-        else
-        {
-            currentTime = (float)stream.ReceiveNext();
-        }
     }
 }
--- Assets/Scripts/Player/RevivePlayer.cs
+++ Assets/Scripts/Player/RevivePlayer.cs
@@ -1,81 +1,40 @@
-using Photon.Pun;
 using UnityEngine;
-using System.Collections;
 
-public class RevivePlayer : MonoBehaviourPun
+public class RevivePlayer : MonoBehaviour
 {
     public float holdTime = 5f;
     private float holdCounter = 0f;
     private bool isHolding = false;
+
     private PlayerDeathManager targetPlayer;
-
     public MedkitTimerUI medkitTimerUI;
-
-    void Start()
-    {
-        if (medkitTimerUI == null)
-        {
-            medkitTimerUI = FindObjectOfType<MedkitTimerUI>();
-            if (medkitTimerUI == null)
-            {
-                Debug.LogError("MedkitTimerUI를 찾을 수 없습니다. 씬에 해당 컴포넌트가 있는지 확인하세요.");
-            }
-        }
-    }
 
     void Update()
     {
         if (targetPlayer != null && Input.GetKey(KeyCode.E))
         {
-            if (!isHolding)
-            {
-                isHolding = true;
-                if (medkitTimerUI != null && medkitTimerUI.photonView.IsMine)
-                {
-                    Debug.Log("RPC 호출 시도 중");
-                    medkitTimerUI.photonView.RPC("RPC_TriggerMedkitTimer", RpcTarget.All);
-                }
-            }
-
+            isHolding = true;
             holdCounter += Time.deltaTime;
+
+            if (medkitTimerUI != null && holdCounter > 0 && !isHolding)
+            {
+                medkitTimerUI.StartMedkitTimer();
+            }
 
             if (holdCounter >= holdTime)
             {
-                // 메딕킷 사용 RPC 호출
-                photonView.RPC("RPC_UseMedkit", RpcTarget.All, targetPlayer.photonView.ViewID);
+                targetPlayer.Revive();
+                Debug.Log("플레이어가 부활했습니다.");
                 ResetHold();
             }
         }
         else if (Input.GetKeyUp(KeyCode.E) || !isHolding)
         {
-            ResetHold();
-        }
-    }
-
-
-
-    [PunRPC]
-    public void RPC_UseMedkit(int targetPlayerViewID)
-    {
-        PhotonView targetPhotonView = PhotonView.Find(targetPlayerViewID);
-        if (targetPhotonView != null)
-        {
-            PlayerDeathManager targetPlayer = targetPhotonView.GetComponent<PlayerDeathManager>();
-            if (targetPlayer != null && targetPlayer.isDead)
+            if (medkitTimerUI != null)
             {
-                targetPlayer.Revive();
-                Debug.Log("플레이어가 부활했습니다.");
+                medkitTimerUI.ResetTimer();
             }
-        }
-    }
-
-    private void ResetHold()
-    {
-        isHolding = false;
-        holdCounter = 0f;
-        if (medkitTimerUI != null)
-        {
-            medkitTimerUI.ResetTimer();
+            ResetHold();
         }
     }
 
@@ -95,5 +54,11 @@ public class RevivePlayer : MonoBehaviourPun
             targetPlayer = null;
             ResetHold();
         }
+    }
+
+    private void ResetHold()
+    {
+        isHolding = false;
+        holdCounter = 0f;
     }
 }
