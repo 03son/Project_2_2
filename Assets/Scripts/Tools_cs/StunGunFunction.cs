132f1950944b4a6af8496866ce43c8ad48de97e4
ljh2002ljh <aeb1119@naver.com>
2024-12-05 00:35:38 +0900

    .

--- Assets/Scripts/Tools_cs/StunGunFunction.cs
+++ Assets/Scripts/Tools_cs/StunGunFunction.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class StunGunFunction : ItemFunction, IItemFunction
 {
     public KeyCode fireKey = KeyCode.Mouse0; // 마우스 좌클릭으로 발사
     public float range = 10f; // 스턴건의 사거리
-    public float stunDuration = 2f; // 적을 5초 동안 멈추게 함
+    public float stunDuration = 5f; // 적을 5초 동안 멈추게 함
     public LayerMask enemyLayer; // 적 레이어 설정
     public LineRenderer lineRenderer; // 라인 렌더러 컴포넌트 추가
     public float laserDuration = 0.5f; // 레이저가 보이는 시간
-
-    private PhotonItem _PhotonItem; // Player_Equip ���� �߰�
+    private PhotonItem _PhotonItem; // Player_Equip 참조 추가
     public AudioClip fireSound; // 발사 소리
     private AudioSource audioSource; // 오디오 소스를 저장할 변수
 
@@ -21,6 +21,7 @@ public class StunGunFunction : ItemFunction, IItemFunction
     {
         playerCamera = Camera.main;
         lineRenderer = GetComponent<LineRenderer>();
+        _PhotonItem = GetComponentInParent<PhotonItem>();
 
         // 오디오 소스 초기화
         audioSource = GetComponent<AudioSource>();
@@ -42,7 +43,15 @@ public class StunGunFunction : ItemFunction, IItemFunction
 
     void FireStunGun()
     {
-
+        // 열쇠 제거 동기화
+        if (_PhotonItem != null && _PhotonItem.photonView != null)
+        {
+            _PhotonItem.RemoveEquippedItem(GetComponent<ItemObject>().item.ItemName);
+            Inventory.instance.RemoveItem(GetComponent<ItemObject>().item.ItemName);
+            Destroy(GetComponentInParent<Player_Equip>().Item);
+            Tesettext();
+            // _PhotonItem.photonView.("RemoveEquippedItem", RpcTarget.All, "Key");
+        }
 
         // 클릭하자마자 발사 소리 재생
         if (audioSource != null && fireSound != null)
