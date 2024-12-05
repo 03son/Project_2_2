66d5f863d6a280402bf03ee820f461ab98609ec2
kjm09139 <164484928+kjm09139@users.noreply.github.com>
2024-12-05 21:46:22 +0900

    사망시 보이스 감지 안되게

--- Assets/Scripts/Monster/CanHearVoiceSource.cs
+++ Assets/Scripts/Monster/CanHearVoiceSource.cs
@@ -12,6 +12,14 @@ public class CanHearVoiceSource : MonoBehaviour
             return;
         if (!other.CompareTag("Player"))
             return; // 플레이어가 아니면 무시
+        // PlayerState 컴포넌트를 가져옴
+        PlayerState playerState = other.GetComponent<PlayerState>();
+        // 플레이어가 사망 상태라면 감지 로직 중단
+        if (playerState != null && playerState.State == PlayerState.playerState.Die)
+        {
+            //Debug.Log("플레이어가 사망 상태입니다. 감지 중단.");
+            return;
+        }
         Mic mic = other.GetComponentInChildren<Mic>();
         if (mic != null)
         {
